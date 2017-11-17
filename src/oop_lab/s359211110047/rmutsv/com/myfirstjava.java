6916a3ed6a365a3d13aa88cee99a094ad047ce57
konkamon <beebiw2428@gmail.com>
2017-11-17 10:31:49 +0700

    Testing upload to Github

---
+++ src/oop_lab/s359211110047/rmutsv/com/myfirstjava.java
@@ -0,0 +1,10 @@
+package oop_lab.s359211110047.rmutsv.com;
+
+public class myfirstjava {
+    public static void main(String[] args) {
+        System.out.println("Hello Wordl, I'm BIW");
+    }// main
+
+
+
+}// class
