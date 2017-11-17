10162cb3658c45f886a0062f3d153a3667db0129
konkamon <beebiw2428@gmail.com>
2017-11-17 11:14:07 +0700

    Successfuly information data

--- src/oop_lab/s359211110047/rmutsv/com/myfirstjava.java
+++ src/oop_lab/s359211110047/rmutsv/com/myfirstjava.java
@@ -3,6 +3,9 @@ package oop_lab.s359211110047.rmutsv.com;
 public class myfirstjava {
     public static void main(String[] args) {
         System.out.println("Hello Wordl, I'm BIW");
+        System.out.println("Information System");
+        System.out.println("Faculty of Management Technololgy");
+        System.out.println("RMUTSV");
     }// main
 
 
