28d72e11cfd4d80455a879a1a514570954925dc2
LAB_COM-MT_1 <beebiw2428@gmail.com>
2017-12-22 09:53:45 +0700

    Input Data To Array

---
+++ src/ooplap5/InputDataToArray.java
@@ -0,0 +1,38 @@
+package ooplap5;
+
+import java.util.Scanner;
+
+public class InputDataToArray {
+    private static int MAX = 5;
+    public static void main (String[] args){
+        int number [] = new int[MAX];
+        System.out.println(number.length);
+        //for
+        number = inputData (number);
+        showData (number);
+        showDataEnchance(number);
+    }//main
+
+    private static void showDataEnchance (int [] number){
+        System.out.println("Data in Array: ");
+        for (int val:number)
+            System.out.print(val+" ");
+    }
+
+
+    private static int[] inputData (int[] number){
+        System.out.println("Data in Array: ");
+        for (int i=0;i<number.length;i++)
+            System.out.print(number[1]+" ");
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an integer");
+        for (int i=0;i<number.length;i++){
+            System.out.print("number["+i+"]:");
+            number[i] = scanner.nextInt();
+        }
+        return number;
+
+    }
+}//class
